1c5caa47da94461751d7f45dccfc57e48d6a18c9
K00268368: Ayyan Muhammed <k00268368@student.tus.ie>
2026-09-22 11:28:40 +0100

    Add Click Attack JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+let score = 0;
+
+const scoreDisplay = document.getElementById("score");
+const title = document.getElementById("title");
+const attackButton = document.getElementById("attackButton");
+const resetButton = document.getElementById("resetButton");
+
+// TODO: create addPoint()
+// TODO: create resetGame()
+// TODO: connect both functions to buttons
